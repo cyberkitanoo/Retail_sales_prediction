5be21d2d7f5372b034d68a82c2d3167abfb91313
Saurabhdimri06 <dimrisaurabh06@gmail.com>
2020-05-05 11:32:44 +0530

    Report Analysis boiler plate

---
+++ WebApp/dashboard/src/components/ReportAnalysis.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class ReportAnalysis extends React.Component{
+    render(){
+        return(
+            <div>
+                This is the report analysis page
+            </div>
+        )
+    }
+}
+
+export default ReportAnalysis
