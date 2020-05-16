caa3175c13e8a56093b1b24625c232bb6313f4ee
unknown <guptae729@gmail.com>
2020-05-16 13:09:24 +0530

    Dashboard Material Updated

--- WebApp/dashboard/src/App.js
+++ WebApp/dashboard/src/App.js
@@ -9,6 +9,7 @@ import { Button } from 'antd'
 import { Menu } from 'antd';
 
 
+
 const { Header, Footer, Sider, Content } = Layout;
 
 
@@ -20,12 +21,12 @@ const { SubMenu } = Menu;
 function App() {
 
   return (
+
     <div className="App">
-
-
     <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-      <Sider style={{background: 'rgb(64, 64, 65)',
-        overflow: 'auto',
+
+      <Sider style={{background: 'rgb(1, 1, 44)',
+        overflow:"auto",
         height: '100vh',
         position: 'fixed',
         left: 0,
@@ -52,33 +53,35 @@ function App() {
       </Menu.Item><br /><br /><br />
       </Menu>
       </Sider>
-
-      <Layout   className="site-layout" style={{ marginLeft: 200, fontSize:30 }}>
-    <Header className="site-layout-background" style={{background:'rgb(64, 64, 65)',  padding: 0, fontSize:35}}>
+      <Header className="site-layout-background" style={{background:'rgb(1, 1, 44)',  padding: 0, fontSize:35}}>
     <Avatar style = {{float:'right'}} icon= {<UserOutlined />} />
     <Title>WELCOME TO RETAIL SALES :)</Title>
     </Header>
 
-      <Content style={{background:'rgba(7, 5, 17, 0.87)', fontSize:20, color:"black"}} 
-      style={{ padding: '0 24px', minHeight: 280, margin: '24px 16px 0', overflow: 'initial', textAlign: 'center' }}>
-      <Title level={3} style={{color:"white"}}>Choose an Option</Title>
+      <Layout   className="site-layout" style={{ marginLeft: 200, fontSize:30 }}>
 
-      <Button type="dashed" style={{background:'rgb(64, 64, 65)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br />   Customer Details   </a> </Button>
+
+      <Content style={{background:'rgba(7, 5, 17, 0.87)', fontSize:20, color:"black"}} 
+      style={{ padding: '0 24px', minHeight: 280, margin: '24px 16px 0', overflow: 'initial', textAlign: 'center' }}
+      >
+      <Title level={3} style={{color:"rgb(1, 1, 44)"}}>Choose an Option</Title>
+
+      <Button type="dashed" style={{background:'rgb(1, 1, 44)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br />   Customer Details   </a> </Button>
       ....
-      <Button type="dashed" style={{background:'rgb(64, 64, 65)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br />  Product Details  </a></Button>
+      <Button type="dashed" style={{background:'rgb(1, 1, 44)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br />  Product Details  </a></Button>
       ....
-      <Button type="dashed" style={{background:'rgb(64, 64, 65)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br />  Sales Data  </a></Button>
+      <Button type="dashed" style={{background:'rgb(1, 1, 44)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br />  Sales Data  </a></Button>
       <span class="tab"></span>
       ....
-      <Button type="dashed" style={{background:'rgb(64, 64, 65)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br /> Reports  </a></Button><br /><br /><br /><br />
+      <Button type="dashed" style={{background:'rgb(1, 1, 44)',fontSize:20}}><a href="/about" style={{color:"white"}} >  Check your <br /> Reports  </a></Button><br /><br /><br /><br />
       
-      <p style={{fontSize:25, color:"white"}}>In today’s world maintaining large records in any company is a very tedious and time consuming task.<br />
+      <p style={{fontSize:25, color:"rgb(5, 5, 32"}}>In today’s world maintaining large records in any company is a very tedious and time consuming task.<br />
         This record in turn needs to be processed and analysed so as to gain valuable insights and analysing <br />
         these for the welfare of the company is again a very tough task .So there is a need for a platform which <br />
         could run predictive analysis on this data . The retail sales company is the target audience so we will <br />
         be focusing on a platform which could predict the stock for the upcoming week based on the historical <br />
         data of the sales.
-        <br /><br /><br /><br /></p>
+        <br /><br /></p>
       </Content>
     </Layout>
 
